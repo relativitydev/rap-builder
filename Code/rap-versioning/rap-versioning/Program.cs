dd015a48141e89e22b931100c947ad6053bf1157
Mark Robustelli <mark.robustelli@relativity.com>
2018-06-11 17:36:46 -0500

    got custom pages working, added rap-version-testing.sln to help while creating.

--- Code/rap-versioning/rap-versioning/Program.cs
+++ Code/rap-versioning/rap-versioning/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
@@ -28,7 +29,7 @@ namespace rap_versioning
 				}
 				else if (args[0].ToLower() == "addref")
 				{
-					returnValue = AddRefrences(path);
+					returnValue = AddAssemblies(path);
 				}
 
 				Console.WriteLine(returnValue);
@@ -41,7 +42,8 @@ namespace rap_versioning
 
 		}
 
-		//TODO: Do not open document twice, thought I needed to because we were going to run twice.
+		//TODO: Do not open document multiple times, thought I needed to because we were going to multiple times with different command options
+		//Should figure out a way to allow option specification for one run only and reduce open and close.
 		static string GetVersion(string fileName)
 		{
 			//should be second parameter
@@ -66,11 +68,12 @@ namespace rap_versioning
 			doc.Load(fileName);
 			doc.SelectSingleNode("/Application/Version").InnerText = newVersionNumber;
 			doc.Save(fileName);
-			AddRefrences(fileName);
+			AddAssemblies(fileName);
+			AddCustomPages(fileName);
 			return newVersionNumber;
 		}
 
-		static string AddRefrences(string fileName)
+		static string AddAssemblies(string fileName)
 		{
 			XmlDocument applicationXML = new XmlDocument();
 			applicationXML.Load(fileName);
@@ -104,5 +107,129 @@ namespace rap_versioning
 			
 			return "something";
 		}
+
+		static string AddCustomPages(string fileName)
+		{
+			NameTable nt = new NameTable();
+			XmlNamespaceManager nsmgr = new XmlNamespaceManager(nt);
+			nsmgr.AddNamespace("ns", "http://schemas.microsoft.com/developer/msbuild/2003"); //default namespace
+
+			XmlDocument applicationXML = new XmlDocument();
+			applicationXML.Load(fileName);
+			XmlNodeList applicationCustomPageNodes = applicationXML.SelectNodes("/Application/CustomPages");
+			
+
+		
+			XmlDocument buildXML = new XmlDocument();
+			string buildXMLPath = fileName.Replace("application.xml", "build.xml");
+			buildXML.Load(buildXMLPath);
+
+			XmlDocument rapBuilderCsprojFile = new XmlDocument();
+			string rapBuilderCsprojFileName = Path.GetDirectoryName(fileName) + @"\" + Path.GetFileName(Path.GetDirectoryName(fileName)) + ".csproj";
+			rapBuilderCsprojFile.Load(rapBuilderCsprojFileName);
+			string rapBuilderOutputRelativityDirectory = rapBuilderCsprojFile.SelectSingleNode(@"ns:Project/ns:PropertyGroup/ns:OutputPath", nsmgr).InnerText;
+			string rapBuilderOutputFullDirectory = Path.Combine(Path.GetDirectoryName(fileName), rapBuilderOutputRelativityDirectory);
+
+			//XmlNodeList customPageNodesList = buildXML.GetElementsByTagName(@"CustomPage");
+			XmlNodeList customPagesNodeList = buildXML.GetElementsByTagName(@"CustomPages");
+			XmlNode customPagesNode = customPagesNodeList[0];
+
+			string customPageZipFileName;
+			string customPageGuid;
+			string customPageVersion;
+			string customPageProjectName;
+			string customPageProjectLocation;
+			string customPageProjectLocationPath;
+			string publishingProfileName;
+			string publishProfilePath;
+			string customPageOutputLocation;
+			string rapBuilderOutputPathForCustomPage;
+
+			XmlNode customPageProjectReferenceNode;
+
+			if (applicationCustomPageNodes != null)
+			{
+				//remove existing children
+				customPagesNode.InnerXml = "";
+
+				//Test for Null
+				foreach (XmlNode customPage in applicationCustomPageNodes[0].SelectNodes("CustomPage"))
+				{
+					customPageZipFileName = customPage.SelectSingleNode("FileName").InnerText;
+					customPageGuid = customPage.SelectSingleNode("Guid").InnerText;
+					customPageVersion = customPage.SelectSingleNode("ApplicationVersion").InnerText;
+					customPageProjectName = Path.GetFileNameWithoutExtension(customPageZipFileName);
+
+					customPageProjectReferenceNode = rapBuilderCsprojFile.SelectSingleNode(@"ns:Project/ns:ItemGroup/ns:ProjectReference[ns:Name[text()='" + customPageProjectName + @"']]", nsmgr);
+					customPageProjectLocation = customPageProjectReferenceNode.Attributes["Include"].Value;
+					customPageProjectLocationPath =
+						Path.GetFullPath(Path.GetFullPath(fileName) + @"\..\.." + customPageProjectLocation);
+
+					XmlDocument customPageProjectCsprojFileXML = new XmlDocument();
+					customPageProjectCsprojFileXML.Load(customPageProjectLocationPath);
+					publishingProfileName = customPageProjectCsprojFileXML.SelectSingleNode(@"/ns:Project/ns:PropertyGroup/ns:AutoDeployPublishProfileName", nsmgr).InnerText;
+
+					publishProfilePath = Path.GetDirectoryName(customPageProjectLocationPath) + @"\Properties\PublishProfiles\" +
+					                     publishingProfileName + @".pubxml";
+					XmlDocument publishProfileXML = new XmlDocument();
+					publishProfileXML.Load(publishProfilePath);
+					customPageOutputLocation = publishProfileXML.SelectSingleNode(@"//ns:Project/ns:PropertyGroup/ns:publishUrl", nsmgr).InnerText;
+					customPageOutputLocation = Path.Combine(Path.GetDirectoryName(customPageProjectLocationPath), customPageOutputLocation);
+					rapBuilderOutputPathForCustomPage = Path.Combine(rapBuilderOutputFullDirectory, customPageProjectName);
+
+					//consider wiping out all files in rapBuilderOutputPathForCustomPage first
+					DirectoryCopy(customPageOutputLocation, rapBuilderOutputPathForCustomPage, true);
+
+
+					XmlElement newAssemblyNode = buildXML.CreateElement("CustomPage");
+					newAssemblyNode.SetAttribute("guid", customPageGuid);
+					newAssemblyNode.InnerText = Path.Combine(rapBuilderOutputRelativityDirectory, customPageProjectName);
+					customPagesNode.AppendChild(newAssemblyNode);
+				}
+
+				buildXML.Save(buildXMLPath);
+			}
+
+			return "something";
+		}
+
+		private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+		{
+			// Get the subdirectories for the specified directory.
+			DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+			if (!dir.Exists)
+			{
+				throw new DirectoryNotFoundException(
+					"Source directory does not exist or could not be found: "
+					+ sourceDirName);
+			}
+
+			DirectoryInfo[] dirs = dir.GetDirectories();
+			// If the destination directory doesn't exist, create it.
+			if (!Directory.Exists(destDirName))
+			{
+				Directory.CreateDirectory(destDirName);
+			}
+
+			// Get the files in the directory and copy them to the new location.
+			FileInfo[] files = dir.GetFiles();
+			foreach (FileInfo file in files)
+			{
+				string temppath = Path.Combine(destDirName, file.Name);
+				file.CopyTo(temppath, true);
+			}
+
+			// If copying subdirectories, copy them and their contents to new location.
+			if (copySubDirs)
+			{
+				foreach (DirectoryInfo subdir in dirs)
+				{
+					string temppath = Path.Combine(destDirName, subdir.Name);
+					DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+				}
+			}
+		}
 	}
+
 }
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # rap-builder
 Alpha version of a Hackathon project aimed at allowing the creation of RAP files within the Visual Studio build. At this time there is no support for this project. If you are interested, reach out to the Developer Experience team at Relativity.
 
+The rap-builder project uses the sample-application project to test with which can be cloned here: git@github.com:relativitydev/sample-application.git .
 
 Note: We are not open-sourcing the source code for kCura.RAPBuilder.exe
